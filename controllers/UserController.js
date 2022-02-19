0b49ea4fb11f1e8c4ea61b4174a60ad99777c310
VictorRevers <victorrkasnowski@gmail.com>
2022-02-19 00:31:18 -0300

    CadastroUsuario

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,6 @@
+const { RegisterSlave } = require("mysql2/lib/commands");
+var User = require("../models/User");
+
 class UserController{
     async index(req, res){}
 
@@ -9,20 +12,34 @@ class UserController{
         if(email == undefined || email == null){
             res.status(400);
             res.json({err: "E-mail inválido!"});
+            return;
         }else if(!emailRegex.test(email)){
             res.status(400);
             res.json({err: "Formato de E-mail inválido!"});
+            return;
         }else if(name == undefined || name == null || name == ""){
             res.status(400);
             res.json({err: "Nome inválido!"});
+            return;
         }else if(password == undefined || password == null){
             res.status(400);
             res.json({err: "Senha inválida!"});
+            return;
         }else if(!letrasMaiusculas.test(password)){
             res.status(400);
             res.json({err: "Senha deve possuir ao menos uma letra maiuscula!"});
+            return;
         }
 
+        var emailExists = await User.findEmail(email);
+
+        if(emailExists){
+            res.status(406);
+            res.json({err: "Email já cadastrado!"});
+            return;
+        }
+
+        await User.new(email, password, name);
 
 
         res.status(200);
